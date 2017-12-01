7646f946ff55076ae265fac7ceeb6732f10318c2
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-12-01 23:47:12 +0300

    some updates

--- spring-boot-course/src/main/java/siendev/corp/courses/springboot/SpringBootCourseApplication.java
+++ spring-boot-course/src/main/java/siendev/corp/courses/springboot/SpringBootCourseApplication.java
@@ -1,4 +1,17 @@
 package siendev.corp.courses.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
 public class SpringBootCourseApplication {
+
+    public static void main(String[] arg) {
+        SpringApplication application = new SpringApplication(SpringBootCourseApplication.class);
+        application.run(arg);
+    }
+
 }
